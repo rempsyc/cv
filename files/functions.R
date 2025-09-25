49eef1526afb4163758d202c7ada93fb41523528
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2025-09-24 22:32:59 -0400

    language freedom

--- files/functions.R
+++ files/functions.R
@@ -1,3 +1,7 @@
+# Define language parameters
+English <- language == "EN"
+French <- language == "FR"
+
 # To format links throughout the CV
 link <- function(txt, url, color = "1976D2") {
   if (!is.null(color)) {
@@ -19,148 +23,175 @@ italics <- function(txt) {
 # Scholar data
 # ------------------------------
 get_scholar_name <- function(scholar.profile) {
-  scholar::get_publications(scholar.profile, 
-                            flush = TRUE) %>% 
-    mutate(author = scholar::get_complete_authors(scholar.profile, pubid)) %>% 
+  scholar::get_publications(scholar.profile, flush = TRUE) %>%
+    mutate(author = scholar::get_complete_authors(scholar.profile, pubid)) %>%
     select(author)
 }
 
-create_grant_entry <- function(Funder_Name,
-                               Project_Name,
-                               Funder_Name_alt = NULL,
-                               Project_Name_alt = NULL,
-                               URL = "",
-                               Date,
-                               Amount,
-                               Program = "") {
-  
+create_grant_entry <- function(
+  Funder_Name,
+  Project_Name,
+  Funder_Name_alt = NULL,
+  Project_Name_alt = NULL,
+  URL = "",
+  Date,
+  Amount,
+  Program = ""
+) {
   a <- tibble(
     Name = paste0("\\textbf{", Funder_Name, "}"),
-    Name_alt = ifelse(is.null(Funder_Name_alt), NA, paste0("\\textbf{", Funder_Name_alt, "}")),
+    Name_alt = ifelse(
+      is.null(Funder_Name_alt),
+      NA,
+      paste0("\\textbf{", Funder_Name_alt, "}")
+    ),
     URL = "",
     Details = "",
     Date = Date,
     Amount = Amount,
     Activity = NA,
     Program = Program,
-    secondary = FALSE)
-  
+    secondary = FALSE
+  )
+
   b <- tibble(
     Name = paste0("\\textit{", Project_Name, "}"),
-    Name_alt = ifelse(is.null(Project_Name_alt), NA, paste0("\\textit{", Project_Name_alt, "}")),
+    Name_alt = ifelse(
+      is.null(Project_Name_alt),
+      NA,
+      paste0("\\textit{", Project_Name_alt, "}")
+    ),
     URL = URL,
     Details = "",
     Date = "",
     Amount = NA,
     Activity = NA,
     Program = Program,
-    secondary = TRUE)
-  
+    secondary = TRUE
+  )
+
   rbind(a, b)
 }
 
 nice_awards <- function(data, theme_color = headcolor, language = "EN") {
-  # data %>% 
-  #   group_by(Program) %>%
-  #   summarize(total = sum(Amount)) %>% 
-  #   add_row(Program = "Total",
-  #           total = sum(data$Amount))
-
+  total <- "Total Amount Offered"
   if (language != "EN") {
-    data <- data %>% 
+    data <- data %>%
       mutate(Name = ifelse(is.na(Name_alt), Name, Name_alt))
+    total <- "Montant total offert"
   }
-    
+
   n <- nrow(data)
   data$Name <- sapply(seq_len(n), function(i) {
     num <- (n + 1 - i)
     paste0(num, ". ", data$Name[i])
-    })
-  
+  })
+
   total_award <- data$Amount
-  
+
   first_year <- min(as.numeric(unlist(strsplit(data$Date, "-"))))
   last_year <- max(as.numeric(unlist(strsplit(data$Date, "-"))))
-  
-  data <- data %>% 
-    add_row(Name = bold("Total Amount Received"),
-            URL = "",
-            Date = bold(paste0(first_year, "-", last_year)),
-            Amount = sum(total_award, na.rm = TRUE),
-            Declined = FALSE,
-            .before = 1) %>% 
-    mutate(Name = link(paste0("\\hspace{0.5cm} ", Name, "\\dotfill"), URL, color = "333333"),
-           Amount = paste0("\\$", scales::label_comma(accuracy = 1)(Amount)),
-           Amount = ifelse(Declined == TRUE,
-                           paste0("(", Amount, ")"),
-                           Amount),
-           Amount = ifelse(Amount == "\\$NA", "—", Amount))
-  
+
+  data <- data %>%
+    add_row(
+      Name = bold(total),
+      URL = "",
+      Date = bold(paste0(first_year, "-", last_year)),
+      Amount = sum(total_award, na.rm = TRUE),
+      Declined = FALSE,
+      .before = 1
+    ) %>%
+    mutate(
+      Name = link(
+        paste0("\\hspace{0.5cm} ", Name, "\\dotfill"),
+        URL,
+        color = "333333"
+      ),
+      Amount = paste0("\\$", scales::label_comma(accuracy = 1)(Amount)),
+      Amount = ifelse(Declined == TRUE, paste0("(", Amount, ")"), Amount),
+      Amount = ifelse(Amount == "\\$NA", "—", Amount)
+    )
+
   # Bold total amount (must do later since we have symbol processing)
-  data[1,"Amount"] <- bold(data[1,"Amount"])
-  
-  data[1,1] <- gsub("333333", theme_color, data[1,1]) #Red: FF0000, but we use same colour as rest
-  
+  data[1, "Amount"] <- bold(data[1, "Amount"])
+
+  data[1, 1] <- gsub("333333", theme_color, data[1, 1]) #Red: FF0000, but we use same colour as rest
+
   brief_entries(data, Name, Date, Amount, .protect = FALSE)
 }
 
 nice_grants <- function(data, theme_color = headcolor, language = "EN") {
-  
+  total <- "Total Amount Offered"
   if (language != "EN") {
-    data <- data %>% 
+    data <- data %>%
       mutate(Name = ifelse(is.na(Name_alt), Name, Name_alt))
+    total <- "Montant total offert"
   }
-  
+
   n <- nrow(data)
   data$Name <- sapply(seq_len(n), function(i) {
     if (i %% 2 == 0) {
       data$Name[i]
     } else {
-      num <- (n + 1 - i) / 2  # 1 → 7, 3 → 6, ..., 13 → 1
-      gsub("\\textbf{", paste0("\\textbf{", num, ". "), data$Name[i], fixed = TRUE)
+      num <- (n + 1 - i) / 2 # 1 → 7, 3 → 6, ..., 13 → 1
+      gsub(
+        "\\textbf{",
+        paste0("\\textbf{", num, ". "),
+        data$Name[i],
+        fixed = TRUE
+      )
     }
   })
-  
+
   total_grant <- data$Amount
-  
+
   first_year <- min(as.numeric(unlist(strsplit(data$Date, "-"))))
   last_year <- max(as.numeric(unlist(strsplit(data$Date, "-"))))
-  
-  data <- data %>% 
-    add_row(Name = bold("Total Amount Offered"),
-            URL = "",
-            Date = bold(paste0(first_year, "-", last_year)),
-            Amount = sum(total_grant, na.rm = TRUE),
-            secondary = FALSE,
-            .before = 1) %>% 
+
+  data <- data %>%
+    add_row(
+      Name = bold(total),
+      URL = "",
+      Date = bold(paste0(first_year, "-", last_year)),
+      Amount = sum(total_grant, na.rm = TRUE),
+      secondary = FALSE,
+      .before = 1
+    ) %>%
     # add_row(Name = "Total Grant + Award Offered",
     #         URL = "",
     #         Date = "",
     #         Amount = sum(total_grant, total_award, na.rm = TRUE),
-    #         secondary = FALSE) %>% 
-    mutate(dotfill = ifelse(secondary, "", "\\dotfill"),
-           Name = link(paste0("\\hspace{0.5cm} ", Name, dotfill), URL, color = "333333"),
-           Amount = paste0("\\$", scales::label_comma(accuracy = 1)(Amount)),
-           Amount = ifelse(Amount == "\\$NA",
-                           "",
-                           Amount))
-  
+    #         secondary = FALSE) %>%
+    mutate(
+      dotfill = ifelse(secondary, "", "\\dotfill"),
+      Name = link(
+        paste0("\\hspace{0.5cm} ", Name, dotfill),
+        URL,
+        color = "333333"
+      ),
+      Amount = paste0("\\$", scales::label_comma(accuracy = 1)(Amount)),
+      Amount = ifelse(Amount == "\\$NA", "", Amount)
+    )
+
   # Bold total amount (must do later since we have symbol processing)
-  data[1,"Amount"] <- bold(data[1,"Amount"])
-  
-  data[1,1] <- gsub("333333", theme_color, data[1,1]) #Red: FF0000, but we use same colour as rest
-  
-  brief_entries(data, 
-                what = Name, 
-                when = Date, 
-                with = Amount, 
-                .protect = FALSE)
-  
+  data[1, "Amount"] <- bold(data[1, "Amount"])
+
+  data[1, 1] <- gsub("333333", theme_color, data[1, 1]) #Red: FF0000, but we use same colour as rest
+
+  brief_entries(data, what = Name, when = Date, with = Amount, .protect = FALSE)
 }
 
-scale_awesomecv_fonts <- function(scale = 1, file_path = "awesome-cv.cls", backup = TRUE) {
-  if (!file.exists(file_path)) stop("File not found: ", file_path)
-  if (backup) file.copy(file_path, paste0(file_path, ".bak"), overwrite = TRUE)
+scale_awesomecv_fonts <- function(
+  scale = 1,
+  file_path = "awesome-cv.cls",
+  backup = TRUE
+) {
+  if (!file.exists(file_path)) {
+    stop("File not found: ", file_path)
+  }
+  if (backup) {
+    file.copy(file_path, paste0(file_path, ".bak"), overwrite = TRUE)
+  }
   lines <- readLines(file_path)
   # Only scale first value, keep {1em}
   pattern <- "\\\\fontsize\\{([0-9.]+)pt\\}\\{1em\\}"
@@ -176,12 +207,12 @@ scale_awesomecv_fonts <- function(scale = 1, file_path = "awesome-cv.cls", backu
   writeLines(lines_scaled, file_path)
   message("Font sizes scaled, baselineskip unchanged (1em).")
 }
-# 
-# 
+#
+#
 # pattern <- "\\\\fontsize\\{([0-9.]+)pt\\}\\{1em\\}"
-# 
+#
 # paste0("fontsize{", scaled, "pt}{1em}")
-# 
+#
 # lines_scaled <- gsubfn::gsubfn(
 #   pattern,
 #   function(size = pattern) {
@@ -197,31 +228,44 @@ format.authors <- function(scholar.profile, author.name) {
   swap_initials <- function(author.name) {
     sub("(.*) (.*)", "\\2, \\1.", trimws(author.name))
   }
-  
+
   pubs <- scholar::get_publications(scholar.profile)
-  pubs %>% 
+  pubs %>%
     strsplit(x = .$author, split = ",") -> pubs2
   lapply(pubs2, function(x) {
     x <- swap_initials(x)
     x[length(x)] <- paste0("& ", x[length(x)])
     x <- paste0(x, collapse = ", ")
     ifelse(startsWith(x, "& "), sub("& ", "", x), x)
-  }
-  ) -> pubs$author
-  
+  }) -> pubs$author
+
   author.name2 <- swap_initials(author.name)
-  
-  pubs %>% 
+
+  pubs %>%
     arrange(desc(year)) %>%
-    mutate(journal = paste0("*", journal, "*"),
-           Publications = paste0(author, " (", year, "). ", 
-                                 title, ". ", journal, ". ", 
-                                 number),
-           Publications = gsub(author.name2, paste0("**", author.name2, "**"), Publications)) %>% 
+    mutate(
+      journal = paste0("*", journal, "*"),
+      Publications = paste0(
+        author,
+        " (",
+        year,
+        "). ",
+        title,
+        ". ",
+        journal,
+        ". ",
+        number
+      ),
+      Publications = gsub(
+        author.name2,
+        paste0("**", author.name2, "**"),
+        Publications
+      )
+    ) %>%
     select(Publications)
 }
 
-number_pubs <- function(pubs, author_bold = NULL){
+number_pubs <- function(pubs, author_bold = NULL) {
   if (!is.null(author_bold)) {
     pubs <- gsub(author_bold, paste0("**", author_bold, "**"), pubs)
   }
@@ -229,3 +273,9 @@ number_pubs <- function(pubs, author_bold = NULL){
   cat(pubs, sep = "\n\n\n")
 }
 
+nice_header <- function(header, header_alt, language = "EN") {
+  if (language != "EN") {
+    header <- header_alt
+  }
+  cat(paste("#", header))
+}
